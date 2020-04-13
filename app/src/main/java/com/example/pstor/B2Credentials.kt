72aa37b76fa0e7643faa4a73ac9d095a2b9361d6
David Francoeur <dfrancoeur04@gmail.com>
2020-04-13 14:13:58 -0400

    Add basic validation

---
+++ app/src/main/java/com/example/pstor/B2Credentials.kt
@@ -0,0 +1,3 @@
+package com.example.pstor
+
+data class B2Credentials(val keyId: String, val key: String)
--- app/src/main/java/com/example/pstor/MainActivity.kt
+++ app/src/main/java/com/example/pstor/MainActivity.kt
@@ -2,6 +2,9 @@ package com.example.pstor
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
+import android.view.View
+import android.widget.EditText
 
 /*
 Required for this application:
@@ -15,5 +18,28 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun onSend(view: View) {
+        val tvKeyId = findViewById<EditText>(R.id.txtKeyId)
+        val tvKey = findViewById<EditText>(R.id.txtKey)
+
+        val creds = validateAndBuildCredentials(tvKeyId, tvKey)
+    }
+
+    fun validateAndBuildCredentials(tvKeyId: EditText, tvKey: EditText): B2Credentials? {
+        if (TextUtils.isEmpty(tvKeyId.text)) {
+            tvKeyId.error = getString(R.string.settings_app_key_id_errors_empty)
+            return null
+        } else {
+            tvKeyId.error = null
+        }
+        if (TextUtils.isEmpty(tvKey.text)) {
+            tvKey.error = getString(R.string.settings_app_key_errors_empty)
+            return null
+        } else {
+            tvKey.error = null
+        }
+        return B2Credentials(tvKeyId.text.toString(), tvKey.text.toString())
     }
 }
